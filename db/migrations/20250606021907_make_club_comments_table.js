68b6109820446bc96813bbb2c43de6ad3a1a8a06
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2025-06-05 22:19:48 -0400

    finished migrations

---
+++ db/migrations/20250606021907_make_club_comments_table.js
@@ -0,0 +1,30 @@
+/**
+ * @param {import('knex')} knex
+ */
+exports.up = async function(knex) {
+    return knex.schema.createTable('club_comments', (table) => {
+      table.increments('id').primary();
+      table
+        .integer('club_id')
+        .notNullable()
+        .references('id')
+        .inTable('clubs')
+        .onDelete('CASCADE');
+      table.string('comment_text').notNullable();
+      table.timestamp('date_created').defaultTo(knex.fn.now());
+      table
+        .integer('user_id')
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE');
+    });
+  };
+  
+  /**
+   * @param {import('knex')} knex
+   */
+  exports.down = async function(knex) {
+    return knex.schema.dropTableIfExists('club_comments');
+  };
+  
